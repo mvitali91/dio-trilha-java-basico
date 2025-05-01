391a2dfdb94fd907cac4e0bd1baf849a788fda01
mvitali91 <mauricio-vitali@hotmail.com>
2025-04-30 21:10:46 -0300

    aula 3004

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,9 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomecompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomecompleto);
+        
+    }
+    
+}
