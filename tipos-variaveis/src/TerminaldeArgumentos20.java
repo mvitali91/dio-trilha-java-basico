ea2e4101b671b999c902ee3ea8722b7ea99946c7
mvitali91 <mauricio-vitali@hotmail.com>
2025-05-12 20:32:44 -0300

    Aula_1205_2025

--- tipos-variaveis/src/TerminaldeArgumentos20.java
+++ tipos-variaveis/src/TerminaldeArgumentos20.java
@@ -1,6 +1,6 @@
 public class TerminaldeArgumentos20 {
     public static void main(String[] args) {
-        System.out.println("Oi, fui executado pelo terminal");
+        System.out.println("Oi Turma, fui executado pelo terminal");
     }
 }
 
